16df47b5a89a6a91aa6710f23fb7f7eff83d5d05
Magdalena Król <leningrand@gmail.com>
2020-04-22 18:39:59 +0200

    Add project files.

---
+++ LINQQueries/Filter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQQueries
+{
+    public class Filter
+    {
+
+        public void MoviesWhere(IEnumerable<Movie> movies)
+        {
+            Console.WriteLine("\n*** Where operator ***\n");
+
+            var moviesAfter2000Where = movies.Where(m => m.Year > 2000);
+            foreach (var movie in moviesAfter2000Where)
+            {
+                Console.WriteLine(movie.Title);
+            }
+        }
+
+        public void MoviesMyLinqFilter(IEnumerable<Movie> movies)
+        {
+
+            Console.WriteLine("\n*** Filter by MyLinq class ***\n");
+
+            var moviesAfter2000MyLinq = movies.Filter(m => m.Year > 2000);
+            var enumerator = moviesAfter2000MyLinq.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Title);
+            }
+        }
+
+        public void MoviesMyLinqCount(IEnumerable<Movie> movies)
+        {
+            Console.WriteLine("\n*** Filter by MyLinq class with Count() ***\n");
+
+            var moviesAfter2000MyLinqCount = movies.Filter(m => m.Year > 2000);
+            Console.WriteLine(moviesAfter2000MyLinqCount.Count());
+            var enumeratorCount = moviesAfter2000MyLinqCount.GetEnumerator();
+            while (enumeratorCount.MoveNext())
+            {
+                Console.WriteLine(enumeratorCount.Current.Title);
+            }
+        }
+
+        public void MoviesMyLinqCountList(IEnumerable<Movie> movies)
+        {
+            Console.WriteLine("\n*** Filter by MyLinq class with Count() and elements on list ***\n");
+
+            var moviesAfter2000MyLinqCountList = movies.Filter(m => m.Year > 2000).ToList();
+            Console.WriteLine(moviesAfter2000MyLinqCountList.Count());
+            var enumeratorCountList = moviesAfter2000MyLinqCountList.GetEnumerator();
+            while (enumeratorCountList.MoveNext())
+            {
+                Console.WriteLine(enumeratorCountList.Current.Title);
+            }
+        }
+
+        public void MoviesMyLinqOrder(IEnumerable<Movie> movies)
+        {
+
+            Console.WriteLine("\n*** Filter by MyLinq class with orderby ***\n");
+
+            var moviesAfter2000MyLinqOrder = movies.Filter(m => m.Year > 2000)
+                                                    .OrderBy(m => m.Raiting);
+            var enumeratorOrder = moviesAfter2000MyLinqOrder.GetEnumerator();
+            while (enumeratorOrder.MoveNext())
+            {
+                Console.WriteLine(enumeratorOrder.Current.Title);
+            }
+        }
+
+        public void MoviesMyLinqOrderQuerySyntax(IEnumerable<Movie> movies)
+        {
+            Console.WriteLine("\n*** Filter by MyLinq class with orderby. Query syntax ***\n");
+
+            var moviesAfter2000MyLinqOrderQuerySyntax = from movie in movies
+                                                        where movie.Year > 2000
+                                                        orderby movie.Raiting
+                                                        select movie;
+            var enumeratorOrderQuerySyntax = moviesAfter2000MyLinqOrderQuerySyntax.GetEnumerator();
+            while (enumeratorOrderQuerySyntax.MoveNext())
+            {
+                Console.WriteLine(enumeratorOrderQuerySyntax.Current.Title);
+            }
+        }
+
+    }
+}
---
+++ LINQQueries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQQueries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Movie> {
+            new Movie { Title = "Dark Knight", Raiting = 8.9f, Year = 2008 },
+            new Movie { Title = "The King's Speach", Raiting = 8.0f, Year = 2010 },
+            new Movie { Title = "Casablanca", Raiting = 8.5f, Year = 1942 },
+            new Movie { Title = "Star Wars V", Raiting = 8.7f, Year = 1980 }
+            };
+
+            var filter = new Filter();
+            filter.MoviesWhere(movies);
+            filter.MoviesMyLinqFilter(movies);
+            filter.MoviesMyLinqCount(movies);
+            filter.MoviesMyLinqCountList(movies);
+            filter.MoviesMyLinqOrder(movies);
+            filter.MoviesMyLinqOrderQuerySyntax(movies);
+
+        }
+    }
+}
